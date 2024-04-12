b63bd29ac73a85e55e4d641e0356dc7917224e4d
Filip Beranek <filip.beranek@yahoo.com>
2024-04-12 09:36:05 +0200

    Adding new helpers

--- helpers/mask2dict.py
+++ helpers/mask2dict.py
@@ -23,21 +23,58 @@ def create_coco_annotation(lo_images, lo_labels, output_folder_path, keyword):
 
 if __name__ == "__main__":
     input_path = pathlib.Path(r"/home/fberanek/Desktop/datasets/segmentation/semantic/new_soiling")
-    # split = -497
-    # Run train
+    
+    input_path_test = input_path / "test"
+    records = open(input_path_test/"test_all_files.txt").readlines()
+    list_of_labels = [f"{input_path_test}{row.split(',')[1].strip()}" for row in records]
+    list_of_iamges = [f"{input_path_test}{row.split(',')[0].strip()}" for row in records]
+    create_coco_annotation(list_of_iamges, list_of_labels, input_path_test, "test_all_files")
+    print(f"len of test records {len(list_of_iamges)}")
+
     input_path_train = input_path / "train"
-    records = open(input_path_train/"correct.txt").readlines()
+    records = open(input_path_train/"train_all_files.txt").readlines()
     list_of_labels = [f"{input_path_train}{row.split(',')[1].strip()}" for row in records]
     list_of_iamges = [f"{input_path_train}{row.split(',')[0].strip()}" for row in records]
-    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "train_correct")
-    records = open(input_path_train/"correct_clear.txt").readlines()
+    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "train_all_files")
+    print(f"len of train_all_files records {len(list_of_iamges)}")
+    records = open(input_path_train/"val_all_files.txt").readlines()
     list_of_labels = [f"{input_path_train}{row.split(',')[1].strip()}" for row in records]
     list_of_iamges = [f"{input_path_train}{row.split(',')[0].strip()}" for row in records]
-    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "train_correct_clear")
-    records = open(input_path_train/"correct_clear_strict.txt").readlines()
+    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "val_all_files")
+    print(f"len of val_all_files records {len(list_of_iamges)}")
+
+    records = open(input_path_train/"train_correct_files.txt").readlines()
     list_of_labels = [f"{input_path_train}{row.split(',')[1].strip()}" for row in records]
     list_of_iamges = [f"{input_path_train}{row.split(',')[0].strip()}" for row in records]
-    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "train_correct_clear_strict")
+    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "train_correct_files")
+    print(f"len of train_correct records {len(list_of_iamges)}")
+    records = open(input_path_train/"val_correct_files.txt").readlines()
+    list_of_labels = [f"{input_path_train}{row.split(',')[1].strip()}" for row in records]
+    list_of_iamges = [f"{input_path_train}{row.split(',')[0].strip()}" for row in records]
+    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "val_correct_files")
+    print(f"len of val_correct records {len(list_of_iamges)}")
+
+    records = open(input_path_train/"train_correct_clear_files.txt").readlines()
+    list_of_labels = [f"{input_path_train}{row.split(',')[1].strip()}" for row in records]
+    list_of_iamges = [f"{input_path_train}{row.split(',')[0].strip()}" for row in records]
+    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "train_correct_clear_files")
+    print(f"len of train_correct_clear records {len(list_of_iamges)}")
+    records = open(input_path_train/"val_correct_clear_files.txt").readlines()
+    list_of_labels = [f"{input_path_train}{row.split(',')[1].strip()}" for row in records]
+    list_of_iamges = [f"{input_path_train}{row.split(',')[0].strip()}" for row in records]
+    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "val_correct_clear_files")
+    print(f"len of val_correct_clear records {len(list_of_iamges)}")
+
+    records = open(input_path_train/"train_correct_clear_strict_files.txt").readlines()
+    list_of_labels = [f"{input_path_train}{row.split(',')[1].strip()}" for row in records]
+    list_of_iamges = [f"{input_path_train}{row.split(',')[0].strip()}" for row in records]
+    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "train_correct_clear_strict_files")
+    print(f"len of train_correct_clear_strict records {len(list_of_iamges)}")
+    records = open(input_path_train/"val_correct_clear_strict_files.txt").readlines()
+    list_of_labels = [f"{input_path_train}{row.split(',')[1].strip()}" for row in records]
+    list_of_iamges = [f"{input_path_train}{row.split(',')[0].strip()}" for row in records]
+    create_coco_annotation(list_of_iamges, list_of_labels, input_path_train, "val_correct_clear_strict_files")
+    print(f"len of val_correct_clear_strict records {len(list_of_iamges)}")
     # Run test
     # input_path_test = input_path / "test"
     # records = open(input_path_train/"correct_clear_strict.txt").readlines()
